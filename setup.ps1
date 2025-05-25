1b3e2d7c067fc78853e9a666e0d18f1173e37b0a
Aload Aload <aload@orgt.eu>
2025-05-26 00:03:56 +0200

    git and windows setup

---
+++ setup.ps1
@@ -0,0 +1,6 @@
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Clipboard" -Name "EnableClipboardHistory" -Value 1 -Type DWord
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Clipboard" -Name "EnableCloudClipboard" -Value 1 -Type DWord
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Clipboard" -Name "CloudClipboardSyncSetting" -Value 1 -Type DWord
+git config --global --add safe.directory D:/aload
+git config --global user.email "aload@orgt.eu"
+git config --global user.name "Aload Aload"
